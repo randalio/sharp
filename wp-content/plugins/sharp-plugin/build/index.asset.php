8d17fffa80fe712d3a7f62bca9ab2fdd2a0b2b85
Randal Pope <randalpope@Randals-MacBook-Air.local>
2024-10-18 14:06:07 -0400

    added sharp plugin

---
+++ wp-content/plugins/sharp-plugin/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '67507e9bb827a7d57339');
---
+++ wp-content/plugins/sharp-plugin/build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '7bf764409843c157c9b6');
